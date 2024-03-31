63d440f4a821aaf524af68b8a88d5e298754aa48
Matthew O'Donnell <matthew.james.od@gmail.com>
2024-03-31 12:01:43 +0100

    added range indifferent ordering

---
+++ Sources/Extensions/Range.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+public extension Range {
+    static func withIndifferentOrdering(_ bound1: Self.Bound, _ bound2: Self.Bound) -> Self {
+        if bound1 <= bound2 {
+            return bound1..<bound2
+        }
+        else {
+            return bound2..<bound1
+        }
+    }
+}
+
+public extension ClosedRange {
+    static func withIndifferentOrdering(_ bound1: Self.Bound, _ bound2: Self.Bound) -> Self {
+        if bound1 <= bound2 {
+            return bound1...bound2
+        }
+        else {
+            return bound2...bound1
+        }
+    }
+}
---
+++ Tests/Extensions/RangeTests.swift
@@ -0,0 +1,23 @@
+import XCTest
+import SwiftUI
+@testable import Extensions
+
+final class RangeTests: XCTestCase {
+    func testIndifferentOrdering() {
+        let range1 = Range.withIndifferentOrdering(-1, 5)
+        let range2 = Range.withIndifferentOrdering(5, -1)
+        
+        XCTAssertEqual(range1, range2)
+    }
+    
+    func testIndifferentOrderingEmpty() {
+        XCTAssertTrue(Range.withIndifferentOrdering(1, 1).isEmpty)
+    }
+    
+    func testClosedIndifferentOrdering() {
+        let range1 = ClosedRange.withIndifferentOrdering(-1, 5)
+        let range2 = ClosedRange.withIndifferentOrdering(5, -1)
+        
+        XCTAssertEqual(range1, range2)
+    }
+}
