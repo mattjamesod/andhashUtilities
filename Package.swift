01e0a195cb9f1d9dd2a19fceb5a6a8e1307bbeb2
Matthew O'Donnell <matthew.james.od@gmail.com>
2024-03-31 20:25:00 +0100

    wrapping hstack

--- Package.swift
+++ Package.swift
@@ -7,13 +7,15 @@ let package = Package(
     name: "andhashUtilities",
     platforms: [
         .iOS("17.0"),
-        .macOS("12.0"),
+        .macOS("13.0"),
     ],
     products: [
         .library(name: "Extensions", targets: ["Extensions"]),
+        .library(name: "Views", targets: ["Views"]),
     ],
     targets: [
         .target(name: "Extensions"),
         .testTarget(name: "ExtensionsTests", dependencies: ["Extensions"]),
+        .target(name: "Views"),
     ]
 )
